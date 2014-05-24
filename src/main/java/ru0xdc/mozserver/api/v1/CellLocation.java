0673148e6a31f03ee6e362ab474ed1acdb9024b8
Alexey Illarionov <littlesavage@rambler.ru>
2014-05-25 00:27:46 +0400

    Google cell location

--- src/main/java/ru0xdc/mozserver/api/v1/CellLocation.java
+++ src/main/java/ru0xdc/mozserver/api/v1/CellLocation.java
@@ -5,7 +5,10 @@ import com.palominolabs.jersey.cors.Cors;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import com.yammer.metrics.annotation.Timed;
+import org.eclipse.jetty.server.AbstractHttpConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru0xdc.mozserver.MozserverConfiguration;
@@ -16,6 +19,13 @@ import ru0xdc.mozserver.model.location.MozillaResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Path("/v1/cell/location")
 @Produces(MediaType.APPLICATION_JSON)
@@ -74,13 +84,13 @@ public class CellLocation {
 			@QueryParam("cid") Integer pCid,
 			@QueryParam("network_radio") Optional<String> pRadio,
 			@QueryParam("psc") Optional<Integer> pPsc) {
-		Client client = Client.create();
-		WebResource r = client.resource("https://location.services.mozilla.com/v1/search")
-				.queryParam("key", mConfiguration.getMozLocServicesKey());
-
 		if (pMcc == null || pMnc == null || pLac == null || pCid == null) {
 			throw new WebApplicationException(Response.status(400).entity(new ru0xdc.mozserver.model.location.CellLocation.Error("Invalid argument")).build());
 		}
+
+		Client client = Client.create();
+		WebResource r = client.resource("https://location.services.mozilla.com/v1/search")
+				.queryParam("key", mConfiguration.getMozLocServicesKey());
 
 		MozillaResponse response;
 		try {
@@ -97,4 +107,67 @@ public class CellLocation {
 		}
 		return new ru0xdc.mozserver.model.location.CellLocation(response.lat, response.lon, (float) response.accuracy);
 	}
+
+	@GET
+	@Path("/google")
+	@Timed
+	@Cors
+	public ru0xdc.mozserver.model.location.CellLocation getGoogle(
+			@QueryParam("mcc") Integer pMcc,
+			@QueryParam("mnc") Integer pMnc,
+			@QueryParam("lac") Integer pLac,
+			@QueryParam("cid") Integer pCid) {
+
+		if (pMcc == null || pMnc == null || pLac == null || pCid == null) {
+			throw new WebApplicationException(Response.status(400).entity(new ru0xdc.mozserver.model.location.CellLocation.Error("Invalid argument")).build());
+		}
+
+		HttpURLConnection connection = null;
+		DataOutputStream os = null;
+		try {
+			URL url = new URL("http://www.google.com/glm/mmap");
+			connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setDoOutput(true);
+			connection.setReadTimeout(25 * 1000);
+			connection.connect();
+			os = new DataOutputStream(connection.getOutputStream());
+			os.writeShort(0x0e);
+			os.writeLong(0);
+			os.writeInt(0);
+			os.writeShort(0);
+			os.writeByte(27);
+			os.writeInt(0); os.writeInt(0); os.writeInt(3);
+			os.writeUTF("");
+			os.writeInt(pCid);
+			os.writeInt(pLac);
+			os.writeInt(pMnc);
+			os.writeInt(pMcc);
+			os.writeInt(0xffffffff);
+			os.writeInt(0);
+			os.flush();
+
+			DataInputStream dis = new DataInputStream(connection.getInputStream());
+			dis.readShort();
+			dis.readByte();
+			int errorCode = dis.readInt();
+			if (errorCode == 0) {
+				int latE6 = dis.readInt();
+				int lonE6 = dis.readInt();
+				return new ru0xdc.mozserver.model.location.CellLocation(latE6 / 1.0e6, lonE6 / 1.0e6);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new WebApplicationException(Response.status(400).entity(
+					new ru0xdc.mozserver.model.location.CellLocation.Error("ioe" + e.getLocalizedMessage())
+			).build());
+		} finally {
+			 if (connection != null) {
+				 connection.disconnect();
+			 }
+		}
+		throw new WebApplicationException(Response.status(404).entity(
+				new ru0xdc.mozserver.model.location.CellLocation.Error("Location not found")).build());
+	}
+
 }
